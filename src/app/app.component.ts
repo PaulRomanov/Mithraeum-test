1945a252979b3c44a4e140a4088081e042bdf289
PaulRomanov <ramosha@mail.ru>
2022-03-28 18:02:46 +0400

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalWindowService } from './services/modal-window.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Mithraeum-test';
+  public title = 'Mithraeum-test';
+  public isShowModal$ = this.modalWindowService.isShowModal$;
+
+  constructor(public modalWindowService: ModalWindowService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalWindowLoginFormModule } from './components/modal-window-login-form/modal-window-login-form.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ModalWindowLoginFormModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
