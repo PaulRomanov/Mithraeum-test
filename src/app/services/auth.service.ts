ee101043786c2264735e1f1b66b28ae467fe70dc
PaulRomanov <ramosha@mail.ru>
2022-03-28 19:28:21 +0400

    add auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+import { map, Observable } from "rxjs";
+import { ResponseLogin } from "../interfaces/interfaces";
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  public isAuthLogin: boolean = false;
+  public userObj = {
+    email: '',
+    password: ''
+  }
+
+  public login(email: string, password: string): Observable<ResponseLogin> {
+
+    const requestURL = '../assets/users/users.json';
+
+    return this.http.get<ResponseLogin>(requestURL).pipe(
+
+      map((data: any) => {
+        const usersList = data["users"];
+        const user = usersList.find(
+          ((usersElement: any) => {
+            if (usersElement.email === email && usersElement.password === password) {
+              this.userObj.email = usersElement.email
+              this.userObj.password = usersElement.password
+
+              localStorage.setItem('email', usersElement.email);
+
+              this.isAuthLogin = true;
+            }
+            return this.userObj;
+          })
+        )
+
+        return user;
+      })
+    );
+
+  }
+
+}
+
+
