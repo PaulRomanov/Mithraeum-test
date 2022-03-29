df48066cf86e7eea2413e452ae6735d205fdc1db
PaulRomanov <ramosha@mail.ru>
2022-03-29 14:42:05 +0400

    add lazy loading, guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: 'main', component: MainPageComponent },
   { path: '', component: MainPageComponent },
-  { path: '**', component: MainPageComponent }
+  { path: '**', component: MainPageComponent },
+  { path: 'loginPage', loadChildren: () => import('./pages/login-page/login-page.module').then(x => x.LoginPageModule), canActivate: [AuthGuard] },
+  { path: 'error', loadChildren: () => import('./pages/error-page/error-page.module').then(x => x.ErrorPageModule), canActivate: [AuthGuard] },
 ];
 
 @NgModule({
